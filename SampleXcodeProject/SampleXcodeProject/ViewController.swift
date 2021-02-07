ca94d958aca03927430108a6c1043f5fe15868b0
17jl0102 <oyata1964kado@gmail.com>
2021-02-07 13:46:36 +0900

    初期プロジェクト投入

---
+++ SampleXcodeProject/SampleXcodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleXcodeProject
+//
+//  Created by 角谷　悠平 on 2021/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
